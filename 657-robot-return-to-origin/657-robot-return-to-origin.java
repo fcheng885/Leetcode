ea6c0a4a50b47a2d2440a9558832fa8a917197eb
Francis <74201038+fcheng885@users.noreply.github.com>
2022-03-16 07:37:23 -0400

    Time: 6 ms (79.96%), Space: 45.8 MB (6.75%) - LeetHub

---
+++ 657-robot-return-to-origin/657-robot-return-to-origin.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        
+        int x = 0;
+        int y = 0;
+        
+        for(char c : moves.toCharArray()){
+            if(c == 'U'){
+                y += 1;
+            }
+            else if(c == 'D'){
+                y -= 1;
+            }
+            else if(c == 'L'){
+                x -= 1;
+            }
+            else{
+                x += 1;
+            }
+        }
+        
+        return x == 0 && y == 0;
+    }
+}
